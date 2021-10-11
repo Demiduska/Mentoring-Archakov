2f300166b7bed61aa71c231e858c8181fbbcce7c
demiduska <andrey.demidenko39@yandex.ru>
2021-10-11 17:42:40 +0200

    установил prettier и исправил

--- src/tutorial-1/components/Profile/ProfileClass.js
+++ src/tutorial-1/components/Profile/ProfileClass.js
@@ -1,21 +1,16 @@
 import React from "react";
 
-
 class ProfileClass extends React.Component {
-
-
-    render() {
-        return (
-            <div className="profile">
-                <h3>
-                    Привет, <strong>{this.props.name}!</strong>
-                </h3>
-                <span>
-                Дата регистрации: {this.props.registredAt}
-            </span>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="profile">
+        <h3>
+          Привет, <strong>{this.props.name}!</strong>
+        </h3>
+        <span>Дата регистрации: {this.props.registredAt}</span>
+      </div>
+    );
+  }
 }
 
 export default ProfileClass;
--- src/tutorial-1/components/Profile/Proflie.js
+++ src/tutorial-1/components/Profile/Proflie.js
@@ -1,23 +1,16 @@
-import './Profile.css';
-
+import "./Profile.css";
 
 function Profile(props) {
+  const { name, registredAt } = props;
 
-    const {
-        name,
-        registredAt
-    } = props;
-
-    return (
-        <div className="profile">
-            <h3>
-                Привет, <strong>{name}!</strong>
-            </h3>
-            <span>
-                Дата регистрации: {registredAt}
-            </span>
-        </div>
-    )
+  return (
+    <div className="profile">
+      <h3>
+        Привет, <strong>{name}!</strong>
+      </h3>
+      <span>Дата регистрации: {registredAt}</span>
+    </div>
+  );
 }
 
 export default Profile;
