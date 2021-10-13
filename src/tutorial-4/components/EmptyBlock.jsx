8034cdf4e3fcc5ca51a148c1b4f1d981df1d75c6
demiduska <andrey.demidenko39@yandex.ru>
2021-10-13 21:52:35 +0200

    Сделал дз №4

---
+++ src/tutorial-4/components/EmptyBlock.jsx
@@ -0,0 +1,19 @@
+function EmptyBlock() {
+  return (
+    <div className="block empty-block">
+      <img
+        src="https://w7.pngwing.com/pngs/83/168/png-transparent-ghost-emoji-sticker-ghost-t-shirt-emoji-cartoon-fictional-character-mobile-phones.png"
+        width="32px"
+        height="32px"
+        alt="Ghost"
+      />
+      <h2>Список фраз пустой</h2>
+      <p>
+        Чтобы в этом списке появилась фраза, тебе необходимо нажать на кнопку
+        “Сгенерировать”
+      </p>
+    </div>
+  );
+}
+
+export default EmptyBlock;
